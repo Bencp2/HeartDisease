eba1d9f4b5fd73c5b777d50711efae90895abe65
Bencp2 <pearlstein.14@osu.edu>
2026-01-06 18:33:55 -0700

    Changed to a another heart disease dataset as the previous did not accurate portray some details relating to the topic.

--- HeartDiseaseDataset.py
+++ HeartDiseaseDataset.py
@@ -1,16 +1,17 @@
 # Install dependencies as needed:
 # pip install kagglehub[pandas-datasets]
+# Source: https://www.kaggle.com/datasets/andrewmvd/heart-failure-clinical-data
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
 
 # Set the path to the file you'd like to load
-file_path = "Heart_Disease_Prediction.csv"
+file_path = "heart_failure_clinical_records_dataset.csv"
 
 
 # Load the latest version
 df = kagglehub.load_dataset(
   KaggleDatasetAdapter.PANDAS,
-  "neurocipher/heartdisease",
+  "andrewmvd/heart-failure-clinical-data",
   file_path,
   # Provide any additional arguments like 
   # sql_query or pandas_kwargs. See the 
@@ -19,4 +20,4 @@ df = kagglehub.load_dataset(
 )
 
 # Create csv file from dataframe
-df.to_csv("HeartDisease/Heart_Disease.csv", index=False)
+df.to_csv("HeartDisease/heart_failure_clinical_records_dataset.csv", index=False)
